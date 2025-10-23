6f6f19b9614907d15282f6bfaa6d65b9d528e3d7
Leidiane Aguiar <leidianec.aguiar@gmail.com>
2025-10-23 19:15:07 -0300

    Adiciona componente item-certificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { SecondaryButtonComponent } from './_components/secondary-button/seconda
     NavbarComponent,
     PrimaryButtonComponent,
     SecondaryButtonComponent,
+    ItemCertificadoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
