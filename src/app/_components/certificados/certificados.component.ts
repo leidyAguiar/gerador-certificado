8a5702f11a5916d0a07d207d9054d31b8529c690
Leidiane Aguiar <leidianec.aguiar@gmail.com>
2025-10-23 19:44:42 -0300

    Adiciona lista de certificados

--- src/app/_components/certificados/certificados.component.ts
+++ src/app/_components/certificados/certificados.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
+import { ItemCertificadoComponent } from '../item-certificado/item-certificado.component';
 
 @Component({
   selector: 'app-certificados',
   standalone: true,
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, ItemCertificadoComponent],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css',
 })
