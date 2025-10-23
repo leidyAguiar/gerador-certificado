60566f62c1d99de3b8ebe5319bb7ef72c19a7024
Leidiane Aguiar <leidianec.aguiar@gmail.com>
2025-10-23 07:11:28 -0300

    commit inicial

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit(): void {
+    this.mensagem();
+  }
+
+  mensagem() {
+    console.log('passou por aqui');
+  }
+}
